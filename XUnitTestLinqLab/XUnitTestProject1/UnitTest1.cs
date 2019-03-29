ae8dbbfb9fb10fe7f3e52753509769be7b5de7ed
Jason Burns <jasonb315@protonmail.com>
2019-03-29 14:02:31 -0700

    data file populated

--- XUnitTestLinqLab/XUnitTestProject1/UnitTest1.cs
+++ XUnitTestLinqLab/XUnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using Xunit;
 
 namespace XUnitTestProject1
 {
+    //Create a program that brings in data from an external file, reads the data, and can filter the data based on specified values.
     public class UnitTest1
     {
         [Fact]
